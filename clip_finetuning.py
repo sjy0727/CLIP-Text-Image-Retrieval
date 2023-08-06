f62947a9f9496984c626e39fcec6c5d072567bbf
sjy0727 <981963252@qq.com>
2023-08-06 16:55:01 +0800

    删除多余的dataset代码
    build_image_caption_pair与ImageCaptionDataset已在image_caption_dataset.py文件中

--- clip_finetuning.py
+++ clip_finetuning.py
@@ -37,55 +37,12 @@ from tqdm import tqdm
 
 # disable warning
 import warnings
+
 warnings.filterwarnings("ignore")
 
 # 加载配置文件
 with open('config.yaml', 'r', encoding='utf-8') as f:
     config = yaml.safe_load(f)
-
-# def build_image_caption_pair(is_train=False):
-#     root_dir = './mini-imagenet/'
-#
-#     with open(os.path.join(root_dir, 'classes_name.json'), 'r') as f:
-#         mini_imagenet_label = json.load(f)
-#
-#     csv_filepath = 'new_train.csv' if is_train else 'new_val.csv'
-#
-#     data = pd.read_csv(os.path.join(root_dir, csv_filepath))
-#
-#     res = []
-#     for idx, (_, row) in enumerate(data.iterrows()):
-#         img_path = os.path.join(root_dir, 'images', row['filename'])
-#         category = int(mini_imagenet_label[row['label']][0])
-#         label = mini_imagenet_label[row['label']][1].replace('_', ' ')
-#         res.append((idx, category, img_path, label))
-#     return res
-#
-#
-# class ImageCaptionDataset(Dataset):
-#     """
-#     输入是图像文件地址列表，标签文字列表，输出是text embeds和image embeds
-#     """
-#
-#     def __init__(self, is_train=False, return_loss=False):
-#         self.data = build_image_caption_pair(is_train=is_train)
-#         self.return_loss = return_loss
-#
-#     def __len__(self):
-#         return len(self.data)
-#
-#     def __getitem__(self, idx):
-#         idx, category, img_path, label = self.data[idx]
-#         img = Image.open(img_path)
-#         return idx, category, img, label
-#
-#     def collate_fn(self, batch):
-#         processor = AutoProcessor.from_pretrained(config['finetune']['checkpoint_dir'])
-#         ids, categories, images, labels = tuple(zip(*batch))  # batch接收的是dataset getitem方法返回值的列表
-#         output = processor(text=labels, images=images, return_tensors='pt', padding=True)
-#         if self.return_loss:
-#             output['return_loss'] = True
-#         return ids, categories, output
 
 
 class DifferentClassSampler(Sampler):
--- embed2db.py
+++ embed2db.py
@@ -45,56 +45,11 @@ def create_milvus_collection(collection_name, dim):
     return collection
 
 
-# def build_image_caption_pair(is_train=False):
-#     root_dir = './mini-imagenet/'
-#
-#     with open(os.path.join(root_dir, 'classes_name.json'), 'r') as f:
-#         mini_imagenet_label = json.load(f)
-#
-#     csv_filepath = 'new_train.csv' if is_train else 'new_val.csv'
-#
-#     data = pd.read_csv(os.path.join(root_dir, csv_filepath))
-#
-#     res = []
-#     for idx, (_, row) in enumerate(data.iterrows()):
-#         img_path = os.path.join(root_dir, 'images', row['filename'])
-#         category = int(mini_imagenet_label[row['label']][0])
-#         label = mini_imagenet_label[row['label']][1].replace('_', ' ')
-#         res.append((idx, category, img_path, label))
-#     return res
-#
-#
-# class ImageCaptionDataset(Dataset):
-#     """
-#     输入是图像文件地址列表，标签文字列表，输出是text embeds和image embeds
-#     """
-#
-#     def __init__(self, is_train=False, return_loss=False):
-#         self.data = build_image_caption_pair(is_train=is_train)
-#         self.return_loss = return_loss
-#
-#     def __len__(self):
-#         return len(self.data)
-#
-#     def __getitem__(self, idx):
-#         idx, category, img_path, label = self.data[idx]
-#         img = Image.open(img_path)
-#         return idx, category, img, label
-#
-#     def collate_fn(self, batch):
-#         processor = AutoProcessor.from_pretrained("openai/clip-vit-base-patch16")
-#         ids, categories, images, labels = tuple(zip(*batch))  # batch接收的是dataset getitem方法返回值的列表
-#         output = processor(text=labels, images=images, return_tensors='pt', padding=True)
-#         if self.return_loss:
-#             output['return_loss'] = True
-#         return ids, categories, output
-
-
 if __name__ == '__main__':
     # 权重路径
     checkpoint_dir = config['model']['checkpoint_dir']
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print('device: ',device)
+    print('device: ', device)
     # 模型加载权重
     # image_encoder = CLIPVisionModelWithProjection.from_pretrained(checkpoint_dir).to(device)
     # text_encoder = CLIPTextModelWithProjection.from_pretrained(checkpoint_dir).to(device)
