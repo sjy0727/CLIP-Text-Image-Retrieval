6d9e91295f08c90ca37a64ac6ff9e18deff0533b
sjy0727 <981963252@qq.com>
2023-08-07 18:32:49 +0800

    Update onnx_model.py
    增加 huggingface 模型类

--- onnx_model.py
+++ onnx_model.py
@@ -9,7 +9,7 @@ import yaml
 import torch
 import onnxruntime
 import numpy as np
-from transformers import AutoProcessor
+from transformers import AutoProcessor, CLIPTextModelWithProjection
 
 # 加载配置文件
 with open('config.yaml', 'r', encoding='utf-8') as f:
@@ -31,3 +31,41 @@ class OnnxModel:
             text_token[i] = text_token[i].astype(np.int64)
         text_embeds = self.session.run(None, text_token)[0]
         return text_embeds / np.linalg.norm(text_embeds, axis=1, keepdims=True)
+
+
+class HfModel:
+    def __init__(self, model_name):
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.model, self.processor = self._load_model_and_processor(model_name)
+        self._warmup()
+
+    def _load_model_and_processor(self, model_name):
+        model = CLIPTextModelWithProjection.from_pretrained(model_name).to(self.device)
+        processor = AutoProcessor.from_pretrained(model_name)
+        return model, processor
+
+    def _warmup(self):
+        input = self.processor(text='warmup text', return_tensors='pt', padding=True).to(self.device)
+        self.model.eval()
+        with torch.no_grad():
+            self.model(**input)
+
+    @classmethod
+    def _empty_cache(self):
+        torch.cuda.empty_cache()
+
+    # 重新加载模型和预处理器
+    def _reload(self, model_name):
+        self._empty_cache()
+        self.model_name = model_name
+        self.model, self.processor = self._load_model_and_processor(model_name)
+
+    def __call__(self, text):
+        text_token = self.processor(text=text, return_tensors='pt', padding=True).to(self.device)
+        self.model.eval()
+        with torch.no_grad():
+            output = self.model(**text_token)
+        # 文本embeds向量归一化
+        output.text_embeds /= output.text_embeds.norm(dim=-1, keepdim=True)
+        text_embeds = output.text_embeds.cpu().numpy()
+        return text_embeds
