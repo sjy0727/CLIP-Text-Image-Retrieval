ea6737e2836b779187f97b436ef9730fc92e9fc7
sjy0727 <981963252@qq.com>
2023-08-13 11:56:30 +0800

    Update db_handler.py

--- db_handler.py
+++ db_handler.py
@@ -4,7 +4,8 @@
 @Author :sunjunyi
 @Time   :2023/8/11 21:06
 """
-import yaml
+import pickle
+import redis
 from config import config
 from pymilvus import MilvusClient, connections, FieldSchema, CollectionSchema, DataType, Collection, utility
 
@@ -81,3 +82,71 @@ class MilvusHandler:
     def drop_collection(collection_name):
         utility.drop_collection(collection_name)
         print('已删除' + collection_name)
+
+
+class RedisHandler:
+    def __init__(self,
+                 host=config.redis.host,
+                 port=config.redis.port,
+                 db=config.redis.db
+                 ):
+
+        self.redis_client = redis.StrictRedis(host=host, port=port, db=db)
+
+    def set(self, key, value, ex=config.redis.expire_time):
+        self.redis_client.set(key, value, ex)
+
+    def get(self, key):
+        result = self.redis_client.get(key)
+        if result:
+            return pickle.loads(result)
+        else:
+            return None
+
+    def mget(self, keys):
+        deserialize_res = []
+        results = self.redis_client.mget(keys)
+        for result in results:
+            if result:
+                deserialize_res.append(pickle.loads(result))
+            else:
+                deserialize_res.append(None)
+        return deserialize_res
+
+    def update_data(self, key, new_value):
+        self.redis_client.set(key, new_value)
+
+    def delete_data(self, key):
+        self.redis_client.delete(key)
+
+
+if __name__ == "__main__":
+    redis_handler = RedisHandler()
+
+    key = "example_key"
+    value = "example_value"
+
+    # 添加数据
+    redis_handler.set(key, value)
+    print(f"添加数据：{key} -> {value}")
+
+    # 获取数据
+    result = redis_handler.get_data(key)
+    print(f"获取数据：{key} -> {result}")
+
+    # 更新数据
+    new_value = "updated_value"
+    redis_handler.update_data(key, new_value)
+    print(f"更新数据：{key} -> {new_value}")
+
+    # 获取更新后的数据
+    result = redis_handler.get_data(key)
+    print(f"获取数据：{key} -> {result}")
+
+    # 删除数据
+    redis_handler.delete_data(key)
+    print(f"删除数据：{key}")
+
+    # 再次获取数据（已删除）
+    result = redis_handler.get_data(key)
+    print(f"获取数据：{key} -> {result}")
