a69492f5c533739f134f8cc0c5126e0a9d9c5ad7
sjy0727 <981963252@qq.com>
2023-08-19 16:59:45 +0800

    Update image_caption_dataset.py
    增加coco，flickr30k的dataset接口

--- image_caption_dataset.py
+++ image_caption_dataset.py
@@ -14,7 +14,20 @@ from PIL import Image
 from transformers import AutoProcessor
 
 
-def build_image_caption_pair(is_train=False):
+def get_labels_and_cates(dataset, is_train):
+    build_dataset_fn = {
+        'mini imagenet': build_mini_imagenet_dataset,
+        'coco': build_coco_dataset,
+        'flickr30k': build_flickr30k
+    }
+    array = build_dataset_fn[dataset](is_train=is_train)
+    array = sorted(array, key=lambda x: int(x[1]))
+    labels = [item[3] for item in array]
+    cates = [item[1] for item in array]
+    return labels, cates
+
+
+def build_mini_imagenet_dataset(is_train=False):
     root_dir = './mini-imagenet/'
 
     with open(os.path.join(root_dir, 'classes_name.json'), 'r') as f:
@@ -33,9 +46,69 @@ def build_image_caption_pair(is_train=False):
     return res
 
 
+def build_coco_dataset(is_train=False):
+    # 设置数据集路径
+    dataDir = './MSCOCO'  # 将路径替换为您的数据集路径
+    dataType = 'train2014' if is_train else 'val2014'  # 或者 'val2014'，取决于您想要加载的数据集部分
+    annFile = os.path.join(dataDir, f'annotations/captions_{dataType}.json')
+
+    # 加载注释文件
+    with open(annFile, 'r') as f:
+        annotations = json.load(f)['annotations']
+
+    # 构建图像ID到描述的映射
+    imgid_to_captions = {}
+    for ann in annotations:
+        img_id = ann['image_id']
+        caption = ann['caption']
+        if img_id in imgid_to_captions:
+            imgid_to_captions[img_id].append(caption)  # 对应多条描述
+        else:
+            imgid_to_captions[img_id] = [caption]
+
+    # 加载图像和对应的描述
+    res = []
+    img_folder = os.path.join(dataDir, dataType)
+    for idx, (img_id, captions) in enumerate(imgid_to_captions.items()):
+        img_info = {
+            'id': img_id,
+            'file_name': f'{img_id:012d}.jpg'  # COCO图像文件名的格式
+        }
+        img_path = os.path.join(img_folder, img_info['file_name'])
+
+        for caption in captions:
+            category = img_id  # img_id作为类别数字
+            res.append((idx, category, img_path, caption))
+    return sorted(res, key=lambda x: int(x[1]))
+
+    # images_and_captions 中包含了每张图像及其对应的多个描述
+    # (idx, category, img_path, label)
+
+
+def build_flickr30k(is_train=False):
+    dataDir = './Flickr30k'
+    annFile = os.path.join(dataDir, 'results_20130124.token')
+    img_folder = os.path.join(dataDir, 'flickr30k-images')
+    annotations = pd.read_table(annFile, sep='\t', header=None, names=['image', 'caption'])
+    captions2images = {caption: image.split('#')[0] for caption, image in
+                       zip(annotations['caption'], annotations['image'])}
+
+    res = []
+    for idx, (image, caption) in enumerate(captions2images.items()):
+        img_path = os.path.join(img_folder, image)
+        category = int(image.split('.')[0])
+        res.append((idx, category, img_path, caption))
+    return res
+
+
 class ImageCaptionDataset(Dataset):
-    def __init__(self, is_train=False, return_loss=False):
-        self.data = build_image_caption_pair(is_train=is_train)
+    def __init__(self, is_train=False, return_loss=False, dataset='mini imagenet'):
+        build_dataset_fn = {
+            'mini imagenet': build_mini_imagenet_dataset,
+            'coco': build_coco_dataset,
+            'flickr30k': build_flickr30k
+        }
+        self.data = build_dataset_fn[dataset](is_train=is_train)
         self.return_loss = return_loss
         self.processor = AutoProcessor.from_pretrained(config.finetune.checkpoint_dir)
 
